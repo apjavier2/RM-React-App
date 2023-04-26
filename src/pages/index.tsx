da4677895ebdbcae174912a5503dd92f881929a2
Pinky Tejeresas <100693180+pinky-sh@users.noreply.github.com>
2023-04-26 13:19:42 +0800

    update code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,32 @@
 import { Row, Button } from "antd";
 import { Caveat } from "next/font/google";
-import { useRouter } from 'next/router';
-import { SearchOutlined } from '@ant-design/icons';
+import { useRouter } from "next/router";
+import { SearchOutlined } from "@ant-design/icons";
 
 const caveat = Caveat({
-  weight: ['400', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-})
-
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function Home() {
   const router = useRouter();
   return (
     <main>
-      <div className={`${caveat.className}`} style={{ fontSize: '25px', textAlign: 'center' }}>
+      <div
+        className={`${caveat.className}`}
+        style={{ fontSize: "25px", textAlign: "center" }}
+      >
         <h2>Rick and Morty</h2>
       </div>
       <Row>
-      <Button style={{marginLeft: '800px', height: '50px', fontSize: '18px'}} onClick={() => router.push(`/charPages`)} icon={<SearchOutlined />}>Browse Characters</Button>
+        <Button
+          style={{ margin: "auto", height: "50px", fontSize: "18px" }}
+          onClick={() => router.push(`/charPages`)}
+          icon={<SearchOutlined />}
+        >
+          Browse Characters
+        </Button>
       </Row>
     </main>
   );
