b628b335d10d82201d1c5d7a04116fc6d49497e4
Pinky Tejeresas <100693180+pinky-sh@users.noreply.github.com>
2023-04-26 08:07:24 +0800

    add button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import CharacList from "./charPages";
-import { Row } from "antd";
+import { Row, Button } from "antd";
 import { Caveat } from "next/font/google";
+import { useRouter } from 'next/router';
+import { SearchOutlined } from '@ant-design/icons';
 
 const caveat = Caveat({
   weight: ['400', '700'],
@@ -8,14 +9,16 @@ const caveat = Caveat({
   subsets: ['latin'],
 })
 
+
 export default function Home() {
+  const router = useRouter();
   return (
     <main>
       <div className={`${caveat.className}`} style={{ fontSize: '25px', textAlign: 'center' }}>
         <h2>Rick and Morty</h2>
       </div>
       <Row>
-        <CharacList />
+      <Button style={{marginLeft: '800px', height: '50px', fontSize: '18px'}} onClick={() => router.push(`/charPages`)} icon={<SearchOutlined />}>Browse Characters</Button>
       </Row>
     </main>
   );
